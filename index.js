ddce18a8b3b719147072b619f28f6d842dd9e967
caorui1230 <55967084+caorui1230@users.noreply.github.com>
2021-02-03 17:54:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 const express = require("express");
 const app = express();
 const router = express.Router();
